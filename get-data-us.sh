86fa3c454900097cf4df7e7d5f15e1a34f2de9a1
Gary Kacmarcik <garykac@gmail.com>
2020-06-06 07:04:21 -0700

    Script to download US/Italy data

---
+++ get-data-us.sh
@@ -0,0 +1,13 @@
+echo "Downloading US/Italy data for $1"
+
+ITALY_URL=https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv
+ITALY_FILE=data/dpc-covid19-ita-andamento-nazionale.csv
+
+US_URL=https://covidtracking.com/api/v1/states/daily.csv
+US_FILE=data/states-daily.csv
+
+curl -o $ITALY_FILE $ITALY_URL
+curl -o $US_FILE $US_URL
+
+git commit -a -m "Snapshot US data for $1"
+
